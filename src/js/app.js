f6d532c9a4300090679d5f0718b15c2ae52d5220
Nathan Furtwangler <nfurtwangler@gmail.com>
2016-10-06 14:29:04 -0700

    Start of camera integration

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,16 @@
-import * as THREE from 'three';
+import Game from './game';
+
 import '../css/app.css';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const game = new Game(window.innerWidth, window.innerHeight);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-window.document.body.appendChild(renderer.domElement);
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 5;
+window.document.body.appendChild(game.domElement);
 
 function render() {
   window.requestAnimationFrame(render);
 
-  // Should be time dependent not frame dependent but who cares this is a demo
-  cube.rotation.x += 0.1 * (60 / 165);
-  cube.rotation.y += 0.1 * (60 / 165);
-
-  renderer.render(scene, camera);
+  game.update();
+  game.draw();
 }
+
 render();
