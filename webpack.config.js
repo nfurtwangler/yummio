dc5975b58dedb356301802154aa3ac4f1e854846
Nathan Furtwangler <nfurtwangler@gmail.com>
2016-10-06 11:21:44 -0700

    ES2017 yall

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ module.exports = {
   entry: './src/js/app.js',
   output: {
     path: './bin/',
+    publicPath: '/bin/',
     filename: 'bundle.js',
     sourceMapFilename: '[file].map',
-    publicPath: '/bin/',
     devtoolModuleFilenameTemplate: 'webpack:///[resource-path]?[loaders]',
   },
   devtool: 'source-map',
